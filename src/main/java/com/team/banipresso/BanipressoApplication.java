f913f95213859d9d60277810feffec7d0fa43537
eunji_jeon <jej940@gmail.com>
2022-09-04 17:17:30 +0900

    최초 커밋

---
+++ src/main/java/com/team/banipresso/BanipressoApplication.java
@@ -0,0 +1,13 @@
+package com.team.banipresso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BanipressoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BanipressoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/team/banipresso/BanipressoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team.banipresso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BanipressoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
